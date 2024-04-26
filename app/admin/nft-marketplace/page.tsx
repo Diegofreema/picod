8f1d490337d63f53a5348476972b431b17a37a77
Diego <diegofreeman78@gmail.com>
2024-04-26 19:50:44 +0100

    updated nft

--- app/admin/nft-marketplace/page.tsx
+++ app/admin/nft-marketplace/page.tsx
@@ -43,16 +43,16 @@ import Card from '../../../components/card/Card';
 import tableDataTopCreators from '../../../views/admin/marketplace/variables/tableDataTopCreators';
 
 // Assets
-import Nft1 from 'img/nfts/Nft1.png';
-import Nft2 from 'img/nfts/Nft2.png';
-import Nft3 from 'img/nfts/Nft3.png';
-import Nft4 from 'img/nfts/Nft4.png';
-import Nft5 from 'img/nfts/Nft5.png';
-import Nft6 from 'img/nfts/Nft6.png';
-import Avatar1 from 'img/avatars/avatar1.png';
-import Avatar2 from 'img/avatars/avatar2.png';
-import Avatar3 from 'img/avatars/avatar3.png';
-import Avatar4 from 'img/avatars/avatar4.png';
+import Nft1 from '/img/nfts/Nft1.png';
+import Nft2 from '/img/nfts/Nft2.png';
+import Nft3 from '/img/nfts/Nft3.png';
+import Nft4 from '/img/nfts/Nft4.png';
+import Nft5 from '/img/nfts/Nft5.png';
+import Nft6 from '/img/nfts/Nft6.png';
+import Avatar1 from '/img/avatars/avatar1.png';
+import Avatar2 from '/img/avatars/avatar2.png';
+import Avatar3 from '/img/avatars/avatar3.png';
+import Avatar4 from '/img/avatars/avatar4.png';
 import AdminLayout from '../../../layouts/admin';
 
 export default function NftMarketplace() {
