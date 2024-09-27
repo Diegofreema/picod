da7c2c8622787c7e43d364e990ec89686b9e69ee
Chibueze <icukaegbu@gmail.com>
2024-09-27 11:30:45 +0100

    changes made again

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -5,12 +5,9 @@ import React, { ReactNode } from 'react';
 import Footer from '../../components/footer/FooterAdmin';
 import SiteNavBar from './_component/Navbar';
 import FixedPlugin from '../../components/fixedPlugin/FixedPlugin';
-import { useSearchParams } from 'next/navigation';
 
 export default function AppLayout({ children }: { children: ReactNode }) {
   const bg = useColorModeValue('secondaryGray.300', 'navy.900');
-  const searchParams = useSearchParams();
-  console.log(searchParams.get('from'));
 
   return (
     <Box bg={bg} w="100%">
